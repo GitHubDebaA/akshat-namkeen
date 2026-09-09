b4cc4f5c2199336390454e5564fb368cd09145cb
GitHubDebaA <bapi98deba@gmail.com>
2026-09-10 02:26:55 +0530

    added product variant -update collection details

--- store/cart.ts
+++ store/cart.ts
@@ -70,7 +70,8 @@ export const useCart = create<CartStore>()(
             closeCart: () => set({ isOpen: false }),
             toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
 
-            total: () => get().items.reduce((sum, i) => sum + i.product.price * i.quantity, 0),
+            // total: () => get().items.reduce((sum, i) => sum + i.product.price * i.quantity, 0),
+            total: () => get().items.reduce((sum, i) => sum + 1 * i.quantity, 0),
             itemCount: () => get().items.reduce((sum, i) => sum + i.quantity, 0),
         }),
         { name: "akshat-namkeen-cart" }
